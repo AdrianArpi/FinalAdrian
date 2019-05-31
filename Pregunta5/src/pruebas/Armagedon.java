597397eb5734ae1a97190dee08022eb6fdc74a1d
Adrian Arpi <arpiadrian5@gmail.com>
2019-05-31 08:31:02 +0200

    Creacion de codigo

---
+++ Pregunta5/src/pruebas/Armagedon.java
@@ -0,0 +1,46 @@
+package pruebas;
+
+public class Armagedon {
+
+	static class Muerte {String nombre ="Muerte";
+		public Muerte(){super();};
+		public String Invocar() {return "El fin se acerca, soy: ";};
+
+}
+	
+	static class Guerra {String nombre ="Guerra";
+	public Guerra(){super();};
+	public String Invocar() {return "El fin se acerca, soy: ";};
+
+}
+	
+	static class Peste {String nombre ="Peste";
+	public Peste(){super();};
+	public String Invocar() {return "El fin se acerca, soy: ";};
+
+}
+	
+	static class Hambre {String nombre ="Hambre"; 
+	public Hambre(){super();};
+	public String Invocar() {return "El fin se acerca, soy: ";};
+
+}
+	
+	public static void main(String[] args) {
+		
+		Muerte	jinete1= new Muerte();
+		Guerra	jinete2= new Guerra();
+		Peste	jinete3= new Peste();
+		Hambre	jinete4= new Hambre();
+		
+		System.out.println(jinete1.Invocar());
+		System.out.println(jinete1.nombre());
+		System.out.println(jinete2.Invocar());
+		System.out.println(jinete2.nombre());
+		System.out.println(jinete3.Invocar());
+		System.out.println(jinete3.nombre());
+		System.out.println(jinete4.Invocar());
+		System.out.println(jinete4.nombre());
+	}
+
+}
